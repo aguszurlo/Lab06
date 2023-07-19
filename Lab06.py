874d12e7c984418a034c5c5e9319306e006a4c91
Agustin Zurlo <aguszurlo@Aguss-MacBook-Pro.local>
2023-07-19 17:32:00 -0500

    Pushed Lab06.py to GitHub Repository

---
+++ Lab06.py
@@ -0,0 +1,43 @@
+def menu():
+    global option
+    print("Menu\n"
+          "-------------\n"
+          "1. Encode\n"
+          "2. Decode\n"
+          "3. Quit\n")
+    option = int(input("Please enter an option: "))
+    return option
+
+
+def encode():
+    global new_password
+    new_password = str("")
+    password = str(input("Please enter your password to encode: "))
+    if len(password) != 8 or any(char.isalpha() for char in password):
+        print("Password must be 8 digits long and can only contain numbers.")
+        encode()
+    else:
+        for char in password:
+            new_char = (int(char) + 3) % 10
+            new_password = new_password + str(new_char)
+        print("Your password has been encoded and stored!\n")
+    return new_password
+
+
+if __name__ == '__main__':
+    option = int()
+    new_password = str("")
+    menu()
+    while True:
+        if option < 1 or option > 3:
+            print("Error. Input must be '1', '2', or '3'\n")
+            menu()
+            continue
+        elif option == 1:
+            encode()
+            print("Encoded password:", new_password, end="\n\n")
+            menu()
+        elif option == 2:
+            menu()
+        elif option == 3:
+            break
